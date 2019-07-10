9db3c81045e0f3bbe373c812933c35c2f0ca3d43
Daniel Stevenson <dstevensonpc@gmail.com>
2019-07-10 18:16:31 -0400

    changes

---
+++ AspCoreDemoApp/Pages/EditChannel.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspCoreDemoApp.Pages
+{
+    public class EditChannelModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
