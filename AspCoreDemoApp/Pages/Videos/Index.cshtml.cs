70eb90b3a6800e2fd98155db56b14e543d251eb5
Daniel Stevenson <dstevensonpc@gmail.com>
2019-07-10 20:39:51 -0400

    changes

--- AspCoreDemoApp/Pages/Videos/Index.cshtml.cs
+++ AspCoreDemoApp/Pages/Videos/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspCoreDemoApp.Core;
+using AspCoreDemoApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,30 @@ namespace AspCoreDemoApp.Pages.Videos
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IData<Channel> channelData;
+        private readonly IData<Video> videoData;
 
+        public IEnumerable<Video> Videos { get; set; }
+        public Channel Channel { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+        public IndexModel(IData<Channel> channelData, IData<Video> videoData)
+        {
+            this.channelData = channelData;
+            this.videoData = videoData;
+        }
+        public IActionResult OnGet(int id)
+        {
+            Channel = channelData.GetById(id);
+
+            if (Channel == null)
+            {
+                return BadRequest();
+            }
+
+            Videos = videoData.GetItems(SearchTerm).Where(v => v.ChannelId == id);
+
+            return Page();
         }
     }
 }
