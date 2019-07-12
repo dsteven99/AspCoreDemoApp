ab3d78cdc141271370a8efa847e989e70018076f
dsstevenson <stevenson@millercanfield.com>
2019-07-12 09:58:41 -0400

    changes

--- AspCoreDemoApp/Pages/Videos/DeleteVideo.cshtml.cs
+++ AspCoreDemoApp/Pages/Videos/DeleteVideo.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspCoreDemoApp.Core;
+using AspCoreDemoApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,38 @@ namespace AspCoreDemoApp.Pages.Videos
 {
     public class DeleteVideoModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IData<Video> videoData;
 
+        public Video Video { get; set; }
+
+        public DeleteVideoModel(IData<Video> videoData)
+        {
+            this.videoData = videoData;
+        }
+        public IActionResult OnGet(int id)
+        {
+            Video = videoData.GetById(id);
+
+            if(Video == null)
+            {
+                return BadRequest();
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            var video = videoData.Delete(id);
+            videoData.Commit();
+
+            if(video == null)
+            {
+                return new StatusCodeResult(500);
+            }
+
+            TempData["Message"] = "Video deleted!";
+            return RedirectToPage("./Index", new { id = video.ChannelId });
         }
     }
 }
