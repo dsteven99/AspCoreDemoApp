88912d815e1716a1ca363614e725f92a102529ee
dsstevenson <stevenson@millercanfield.com>
2019-07-12 09:41:15 -0400

    changes

--- AspCoreDemoApp/Pages/Videos/EditVideo.cshtml.cs
+++ AspCoreDemoApp/Pages/Videos/EditVideo.cshtml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using AspCoreDemoApp.Core;
+using AspCoreDemoApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,50 @@ namespace AspCoreDemoApp.Pages.Videos
 {
     public class EditVideoModel : PageModel
     {
-        public void OnGet()
+        private readonly IData<Channel> channelData;
+        private readonly IData<Video> videoData;
+
+        public Channel Channel { get; set; }
+
+        [BindProperty]
+        public Video Video { get; set; }
+        public EditVideoModel(IData<Channel> channelData, IData<Video> videoData)
         {
+            this.channelData = channelData;
+            this.videoData = videoData;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Video = videoData.GetById(id);
+
+            if(Video == null)
+            {
+                return BadRequest();
+            }
+
+            Channel = channelData.GetById(Video.ChannelId);
+
+            return Page();
+        }
+
+        public IActionResult OnPost(int channelId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            videoData.Update(Video);
+            videoData.Commit();
+
+            Channel = channelData.GetById(channelId);
+            Channel.LastModified = DateTime.Now;
+          
+            channelData.Update(Channel);
+            channelData.Commit();
+
+            return RedirectToPage("./Index", new { id = Channel.Id });
 
         }
     }
