bb2448bb6c17e634b7990b258618cc5ca525fbc4
dsstevenson <stevenson@millercanfield.com>
2019-07-24 12:14:33 -0400

    changes

--- AspCoreDemoApp.Test/IntegrationTest.cs
+++ AspCoreDemoApp.Test/IntegrationTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using AspCoreDemoApp.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -23,7 +24,37 @@ namespace AspCoreDemoApp.Test
         public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
         {
             // Arrange
-            var client = factory.CreateClient();
+            var client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+
+                    using (var scope = serviceProvider.CreateScope())
+                    {
+                        var scopedServices = scope.ServiceProvider;
+                        var db = scopedServices
+                            .GetRequiredService<VideoDbContext>();
+                        var logger = scopedServices
+                            .GetRequiredService<ILogger<IntegrationTests>>();
+
+                        try
+                        {
+                            Utilities.InitializeDbForTests(db);
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.LogError(ex, "An error occurred seeding " +
+                                "the database with test messages. Error: " +
+                                ex.Message);
+                        }
+                    }
+                });
+            })
+       .CreateClient(new WebApplicationFactoryClientOptions
+       {
+           AllowAutoRedirect = false
+       });
 
             // Act
             var response = await client.GetAsync(url);
