99d0429b8bd5cece95083e20b5ac5d76e43265e0
Daniel Stevenson <dstevensonpc@gmail.com>
2019-07-28 22:30:33 -0400

    changes

--- AspCoreDemoApp/Api/VideoController.cs
+++ AspCoreDemoApp/Api/VideoController.cs
@@ -20,9 +20,9 @@ namespace AspCoreDemoApp.Api
             this.videoData = videoData;
         }
 
-        [Route("{id}")]
+        [Route("next/{id}")]
         [HttpGet]
-        public IActionResult Get(int id)
+        public IActionResult GetNext(int id)
         {
             var video = videoData.GetById(id);
             if(video == null)
@@ -43,6 +43,32 @@ namespace AspCoreDemoApp.Api
             {
                 //its the last video in collection so return first video
                 return Ok(videos[0].Id);
+            }
+        }
+
+        [Route("previous/{id}")]
+        [HttpGet]
+        public IActionResult GetPrevious(int id)
+        {
+            var video = videoData.GetById(id);
+            if (video == null)
+            {
+                return BadRequest();
+            }
+
+            List<Video> videos = videoData.GetItems("").Where(v => v.ChannelId == video.ChannelId).ToList();
+
+            int index = videos.FindIndex(v => v.Id == video.Id);
+
+            if (index > 0)
+            {
+                //return previous video in collection
+                return Ok(videos[index - 1].Id);
+            }
+            else
+            {
+                //its the first video in collection so return last video
+                return Ok(videos[videos.Count - 1].Id);
             }
         }
     }
